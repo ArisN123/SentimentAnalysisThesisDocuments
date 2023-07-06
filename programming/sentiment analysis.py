1aa4c635a56e7034fd60750eaf1ada1ce790c1d4
Aris <117669709+ArisN123@users.noreply.github.com>
2023-07-06 20:52:49 +0200

    sentiment analysis.py

--- programming/sentiment analysis.py
+++ programming/sentiment analysis.py
@@ -1,9 +1,12 @@
 import pandas as pd
-from nltk.tokenize import sent_tokenize, word_tokenize
+import matplotlib.pyplot as plt
+from nltk.tokenize import sent_tokenize, word_tokenize, pos_tag
 from spellchecker import SpellChecker
+from collections import defaultdict
 
 # Load the NRC EmoLex Lexicon
-emolex_df = pd.read_csv('NRC-Emotion-Lexicon-Wordlevel-v0.92.csv')
+emolex_df = pd.read_csv('NRC-UPDATED.csv')
+# Alter if necessary to add words that fit the specific context of the discussion
 
 # Convert the lexicon to a dictionary for efficiency
 emolex_dict = emolex_df.pivot(index='word', 
@@ -11,8 +14,17 @@ emolex_dict = emolex_df.pivot(index='word',
                               values='association').to_dict()
 
 # Create lists of simple intensifiers and negations
-intensifiers = ['very', 'extremely', 'absolutely', 'totally']
-negations = ['not', 'never', 'no']
+intensifiers = [
+    'very', 'extremely', 'absolutely', 'totally', 'incredibly', 'rather', 'quite', 'highly', 
+    'exceptionally', 'especially', 'particularly', 'super', 'seriously', 'remarkably', 
+    'majorly', 'most', 'more', 'so', 'really'
+]
+
+negations = [
+    'not', 'never', 'no', 'none', 'nobody', 'nothing', 'neither', 'nowhere', 'never',
+    'hardly', 'scarcely', 'barely', 'doesn’t', 'isn’t', 'wasn’t', 'shouldn’t', 'wouldn’t', 
+    'couldn’t', 'won’t', 'can’t', 'don’t'
+]
 
 # Initialize spell checker
 spell = SpellChecker()
@@ -43,24 +55,51 @@ def sentiment_analysis(text):
             for emotion in emotions:
                 emotions[emotion] += emolex_dict[corrected_word][emotion] * intensity
     
-    return emotions
+    # Determine whether the sentiment is positive
+    positive = sum(emotions.values()) > 0
+
+    return positive
 
 # Load your review data
 reviews_df = pd.read_csv('reviews.csv')
 
-# Create an empty DataFrame to store the sentiment for each sentence
-sentences_df = pd.DataFrame(columns=['review', 'sentence', 'sentiment'])
+# Initialize a defaultdict to count nouns
+noun_counter = defaultdict(lambda: defaultdict(int))
 
-# Tokenize each review into sentences and analyze sentiment
+# Tokenize each review into sentences, analyze sentiment, and identify nouns
 for index, row in reviews_df.iterrows():
     sentences = sent_tokenize(row['review'])
     for sentence in sentences:
-        sentiment = sentiment_analysis(sentence)
-        sentences_df = sentences_df.append({
-            'review': row['review'],
-            'sentence': sentence,
-            'sentiment': sentiment
+        positive = sentiment_analysis(sentence)
+
+        # Determine the sentiment label
+        sentiment_label = 'positive' if positive else 'negative'
+
+        tagged_words = pos_tag(word_tokenize(sentence))
+        nouns = [word for word, pos in tagged_words if pos in ['NN', 'NNS']]
+
+        for noun in nouns:
+            noun_counter[noun][sentiment_label] += 1
+
+# Create a DataFrame to store the results
+results_df = pd.DataFrame(columns=['noun', 'count', 'sentiment'])
+
+for noun, sentiment_counter in noun_counter.items():
+    if sentiment_counter['positive'] > 0:
+        results_df = results_df.append({
+            'noun': noun,
+            'count': sentiment_counter['positive'],
+            'sentiment': 'positive'
         }, ignore_index=True)
 
-# Preview the data
-print(sentences_df.head())
+    if sentiment_counter['negative'] > 0:
+        results_df = results_df.append({
+            'noun': noun,
+            'count': sentiment_counter['negative'],
+            'sentiment': 'negative'
+        }, ignore_index=True)
+
+# Export to CSV
+results_df.to_csv('sentiment_results.csv', index=False)
+
+
