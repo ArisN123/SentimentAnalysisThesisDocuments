58bca76ea94bdf8bf4f164743d76bc10b310aad7
Aris <117669709+ArisN123@users.noreply.github.com>
2023-07-07 12:37:04 +0200

    Update sentiment analysis model (to apply a score to sentences).py

--- Code/sentiment analysis model (to apply a score to sentences).py
+++ Code/sentiment analysis model (to apply a score to sentences).py
@@ -2,15 +2,12 @@ import pandas as pd
 from nltk.tokenize import word_tokenize
 from spellchecker import SpellChecker
 
-# Load the NRC EmoLex Lexicon
 emolex_df = pd.read_csv('NRC-UPDATED.csv')
 
-# Convert the lexicon to a dictionary for efficiency
 emolex_dict = emolex_df.pivot(index='word', 
                               columns='emotion', 
                               values='association').to_dict()
 
-# Create lists of intensifiers and negations
 intensifiers = [
     'very', 'extremely', 'absolutely', 'totally', 'incredibly', 'rather', 'quite', 'highly', 
     'exceptionally', 'especially', 'particularly', 'super', 'seriously', 'remarkably', 
@@ -24,26 +21,19 @@ negations = [
 ]
 
 
-# Initialize spell checker
 spell = SpellChecker()
 
-# Begin sentiment analysis process
 def sentiment_analysis(text):
-    # Initialize scores for each emotion to 0
     emotions = {emotion: 0 for emotion in emolex_dict.keys()}
     
-    # Tokenize the text into words
     words = word_tokenize(text.lower())
 
-    # For each word, check for intensifiers or negations before it (2-word range), and add the value it represents for each emotion
     for i, word in enumerate(words):
         corrected_word = spell.correction(word)
         
         if corrected_word in emolex_dict:
-            # Initialize intensity
             intensity = 1
             
-            # Check for intensifier and/or negation up to two words before the word
             for j in range(max(0, i-2), i):
                 if words[j] in intensifiers:
                     intensity *= 2
@@ -53,18 +43,14 @@ def sentiment_analysis(text):
             for emotion in emotions:
                 emotions[emotion] += emolex_dict[corrected_word][emotion] * intensity
 
-    # Calculate sentiment score as the sum of emotion scores normalized to range -1 to 1
     sentiment_score = sum(emotions.values())
     sentiment_score = -1 if sentiment_score < 0 else 1 if sentiment_score > 0 else 0
 
     return sentiment_score
 
-# Load Twitter data for analysis
 # Switch csv file with AmazonReviews.csv when analysing the Amazon data
 df = pd.read_csv('McDoTwitterdata.csv')
 
-# Apply sentiment analysis to each row and store the results in a new column
 df['sentiment_score'] = df['text_column'].apply(sentiment_analysis)
 
-# Save the DataFrame with the sentiment scores back to a csv file
 df.to_csv('reviews_with_sentiment.csv', index=False)
